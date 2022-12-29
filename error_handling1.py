9cc368397e6c19752554688551a00020dac6e19b
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-29 20:52:39 +0530

    Add files via upload

---
+++ error_handling1.py
@@ -0,0 +1,44 @@
+'''
+DATE:29TH DEC 2022
+DAY: THURSDAY
+TOPIC: ERROR_HANDLING
+AUTHOR:RAMA BHARGAvi
+'''
+# print("python")
+# print("core python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a)
+#     print(a/0)
+# except ZeroDivisionError:
+#     print('not ok divided by zero')
+# print("advance analytics")
+# print("python analytics")
+# print("python")
+# print("core python")
+# a=5
+# b='6'
+# try:
+#     print(a/5)
+#     print(a/0)
+#     print("hello")
+# except ZeroDivisionError:
+#     print('not ok divided by zero')
+#     print("advance analytics")
+#     print("python analytics") 
+print("python")
+print("core python")
+a=5
+b='6'
+try:
+    print(a/5)
+    print(a/0)
+    print(a+a)
+except ZeroDivisionError:
+    print('not ok divided by zero')
+    print("advance python")
+    print("python analytics")
+
+
