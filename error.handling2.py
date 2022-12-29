f29324d1e8b64081ecb90990da895bd6f3ecfef0
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-29 21:09:20 +0530

    Add files via upload

---
+++ error.handling2.py
@@ -0,0 +1,42 @@
+'''
+DATE:29TH DEC 2022
+DAY: THURSDAY
+TOPIC: ERROR_HANDLING
+AUTHOR:RAMA BHARGAvi
+'''
+print("python")
+print("core python")
+a=5
+b='6'
+try:
+    print(a/5)
+    print(a/0)
+    print(b+"hello")
+except ZeroDivisionError:
+        print('not ok divided by zero')
+print("advance python")
+print("python analytics")
+print("python")
+print("core python")
+a=5
+b='6'
+try:
+    print(a/5)
+    print(a/0)
+    print('')
+except ZeroDivisionError:
+    print('not ok divided by zero')
+    print("advance python")
+    print("python analytics")
+print("python")
+print("core python")
+a=5
+b='6'
+try:
+    print(a/5)
+except ZeroDivisionError:
+        print('not ok divided by zero')
+finally:
+        print("its ok next time don't divide by zero")
+print("advance python")
+print("python analytics")
